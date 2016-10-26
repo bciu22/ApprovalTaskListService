f58e8176c07c192e4748e1af662904d7a7c271d2
Charles <crossan007@gmail.com>
2016-10-26 10:30:15 -0400

    Fix Character Escaping
    Fix issue where quotes cause comment strings to be cut off

--- TaskListService/TaskListService.cs
+++ TaskListService/TaskListService.cs
@@ -8,12 +8,42 @@ using System.Diagnostics;
 using System.Data;
 using System.Text.RegularExpressions;
 using System.Web;
+using System.Collections;
 
 namespace TaskListService
 {
     [WebService(Namespace = "http://bucksiu.org/webservices/TaskListWebService")]
     public class TaskListService : WebService
     {
+        [WebMethod]
+        public DataSet GetFormApprovalTasks(string TaskListName, string subsiteName, string FormURL)
+        {
+             //gets approvals related to the same workflowitemid for a supplied approval id
+            SPWeb thisSite = SPContext.Current.Site.WebApplication.Sites[subsiteName].OpenWeb();
+            Debug.WriteLine("Found Site " + thisSite.Name.ToString());
+            SPList TaskList = thisSite.Lists.TryGetList(TaskListName);
+            if ( TaskList == null )
+            {
+                throw new Exception("Unable to locate requested Task List: " + TaskListName);
+            }
+            SPQuery query = new SPQuery();
+            query.Query = string.Concat(
+                        "<Where><Contains>",
+                            "<FieldRef Name='WorkflowLink'/>",
+                            "<Value Type='String'>" + FormURL + "</Value>",
+                        "</Contains></Where>");
+
+            query.ViewFields = "<FieldRef Name='WorkflowItemId' />";
+            query.ViewFieldsOnly = true; // Fetch only the data that we need.
+            SPListItemCollection items = TaskList.GetItems(query);
+            var distinctItems = (from SPListItem item in items select item["WorkflowItemId"]).Distinct().ToArray();
+            if (distinctItems.Length != 1 )
+            {
+                throw new Exception("Not exactly one related document.  Please verify the name, and try again.  Related Documents: "+distinctItems.Length.ToString());
+            }
+            string WorkflowItemId = distinctItems[0].ToString();
+            return GetSiteApprovalTasks(subsiteName, TaskListName, int.Parse(WorkflowItemId));
+        }
 
         [WebMethod]
         public DataSet GetRelatedTasks(string TaskListName, int taskID)
@@ -86,29 +116,50 @@ namespace TaskListService
                         foreach (DataRow row in results.Rows)
                         {
                             string pattern = @"ows_FieldName_Comments='(.*?)'";
-                            string extendedProperties = HttpUtility.HtmlDecode(row["ExtendedProperties"].ToString());
-                            MatchCollection comments = Regex.Matches(extendedProperties, pattern);
+                            //string extendedProperties = HttpUtility.HtmlDecode(row["ExtendedProperties"].ToString());  // this should never have been here since we need to escape comment values
+                            string extendedProperties = row["ExtendedProperties"].ToString();
+                            MatchCollection comments = Regex.Matches(extendedProperties, pattern, RegexOptions.Singleline);
                             if (comments.Count > 0)
                             {
 
-                                row["Comments"] = comments[0].Groups[1].Value;
+                                row["Comments"] = HttpUtility.HtmlDecode(comments[0].Groups[1].Value);
                             }
 
                             pattern = @"ows_TaskStatus='(.*?)'";
-                            MatchCollection taskStatus = Regex.Matches(extendedProperties, pattern);
+                            MatchCollection taskStatus = Regex.Matches(extendedProperties, pattern, RegexOptions.Singleline);
                             pattern = @"ows_FieldName_DelegateTo='(.*)<pc:DisplayName>(.*)</pc:DisplayName>";
-                            MatchCollection delegateTo = Regex.Matches(extendedProperties, pattern);
+                            MatchCollection delegateTo = Regex.Matches(extendedProperties, pattern, RegexOptions.Singleline);
                             if (taskStatus.Count > 0)
                             {
                                 if(delegateTo.Count > 0)
                                 {
-                                    row["TaskStatus"] = "Delegate: " + delegateTo[0].Groups[2].Value;
+                                    row["TaskStatus"] = "Delegate: " + HttpUtility.HtmlDecode(delegateTo[0].Groups[2].Value);
                                 }
                                 else
                                 {
-                                    row["TaskStatus"] = taskStatus[0].Groups[1].Value;
+                                    if (taskStatus[0].Groups[1].Value == "ChangeView")
+                                    {
+
+                                        row["TaskStatus"] = "Change Completed. Workflow Restarted";
+                                    }
+                                    else if(taskStatus[0].Groups[1].Value == "ChangeRequest")
+                                    {
+                                        row["TaskStatus"] = "Change Requested";
+                                        pattern = @"ows_FieldName_NewDescription='(.*?)'";
+                                        MatchCollection changeComments = Regex.Matches(extendedProperties, pattern, RegexOptions.Singleline);
+                                        row["Comments"] += HttpUtility.HtmlDecode(changeComments[0].Groups[1].Value);
+                                    }
+                                    else
+                                    {
+                                        row["TaskStatus"] = HttpUtility.HtmlDecode(taskStatus[0].Groups[1].Value);
+                                    }
+                                  
                                 }
                                
+                            }
+                            else
+                            {
+                                row["TaskStatus"] = "Pending";
                             }
                         }
                         results.EndLoadData();
